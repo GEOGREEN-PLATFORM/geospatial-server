5e356c7d196383b163d25921d0de02f0dc4ed08f
Dasha <dashavavilova2002@icloud.com>
2024-12-21 14:47:06 +0300

    init

---
+++ src/test/java/com/example/geospatial_server/GeospatialServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.geospatial_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeospatialServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
