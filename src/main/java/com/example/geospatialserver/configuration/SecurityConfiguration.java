783d1f870231e6eacd0f0143d1ed45c3bca605a5
Dasha <dashavavilova2002@icloud.com>
2025-04-20 23:36:58 +0300

    Add access to some methods without token

--- src/main/java/com/example/geospatialserver/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/geospatialserver/configuration/SecurityConfiguration.java
@@ -13,7 +13,12 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfiguration {
-    private static final String[] ALLOWED_URLS = {"/v3/api-docs/**", "/swagger-ui/**"};
+    private static final String[] ALLOWED_URLS = {"/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/geo/dict/**",
+            "/geo/info/{geoPointId}",
+            "/geo/info/getAll",
+            "/geo/info/getAll/{problemAreaType}"};
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
